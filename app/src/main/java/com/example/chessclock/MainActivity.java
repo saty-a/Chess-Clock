e98a9458ae90a1fad1b8c368b09a98794cd08852
satya-prakashan <satyaprakash6945@gmail.com>
2022-09-28 01:16:22 +0530

    updated ver4

--- app/src/main/java/com/example/chessclock/MainActivity.java
+++ app/src/main/java/com/example/chessclock/MainActivity.java
@@ -2,19 +2,25 @@ package com.example.chessclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout tp,btm;
+    LinearLayout tp,btm,ll;
     TextView tptimer,btmtimer;
-    ImageView Reset,Pause,Play;
+    ImageView Reset,Pause,Play,Settings;
     private static final long START_TIME_IN_MILLIS = 600000;
 
 
@@ -22,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     private CountDownTimer mCountDownTimerTop,mCountDownTimerBottom;
 
     private boolean mTimerRunning,mBTimerRunning,switchClock=true;
-
     private long mTimeLeftInMillisTop = START_TIME_IN_MILLIS;
     private long mTimeLeftInMillisTBottom = START_TIME_IN_MILLIS;
 
@@ -31,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         //Initializations
+        ll=findViewById(R.id.ll);
         tp=findViewById(R.id.top);
         btm=findViewById(R.id.bottom);
         tptimer=findViewById(R.id.uptimer);
@@ -39,15 +47,26 @@ public class MainActivity extends AppCompatActivity {
         Reset=findViewById(R.id.reset);
         Pause=findViewById(R.id.pause);
         Play=findViewById(R.id.play);
+        Settings=findViewById(R.id.settings);
 
         tp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 startTimerBottomclock();
+
                 if (mTimerRunning) {
+                    Vibrate();
                     pauseTimer();
                 }
+            }
+        });
+
+        Settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(MainActivity.this,SettingActivity.class);
+                startActivity(i);
             }
         });
 
@@ -57,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
                 startTimerTopClock();
                 if (mBTimerRunning){
+                    Vibrate();
                     pauseTimerBottom();
                 }
             }
@@ -83,6 +103,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void Vibrate() {
+        // get the VIBRATOR_SERVICE system service
+        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        final VibrationEffect vibrationEffect2;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+
+            // create vibrator effect with the constant EFFECT_CLICK
+            vibrationEffect2 = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+
+            // it is safe to cancel other vibrations currently taking place
+            vibrator.cancel();
+
+            vibrator.vibrate(vibrationEffect2);
+        }
     }
 
     private void startTimerBottomclock() {
